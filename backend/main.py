dff634fe8df7936c65f63bf36d9fd587d10ca186
IOSH <iveninvala7@gmal.com>
2023-12-07 17:32:48 +0300

    Feat 3: add corsmiddleware, include router

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from api.assembly_pc.urls import router as router_asembly_pc
 
-app = FastAPI()
+app = FastAPI(
+     title="Assembly PC"
+)
 
+app.include_router(
+    router_asembly_pc,
+    prefix="/asm-pc",
+    tags=["Assembly PC"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# All hosts that can access our api
+origins = [
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)

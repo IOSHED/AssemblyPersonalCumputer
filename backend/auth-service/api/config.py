a8a52661e25bc922b3ec3d426c8a1f99835a2559
IOSH <iveninvala7@gmal.com>
2023-12-10 18:23:31 +0300

    Init 18: auth service

---
+++ backend/auth-service/api/config.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
--- backend/auth-service/api/main.py
+++ backend/auth-service/api/main.py
@@ -1,9 +1,31 @@
 from fastapi import FastAPI
-# from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
+
+from api.auth.urls import router as router_auth
 
 
 app = FastAPI(
-    title="Assembly PC",
+    title="Auth",
     openapi_url="/api/v1/auth/openapi.json",
     docs_url="/api/v1/auth/docs"
+)
+
+app.include_router(
+    router_auth,
+    prefix="/api/v1/auth",
+    tags=["Auth"],
+)
+
+# All hosts that can access our api
+origins = [
+    "http://127.0.0.1:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
 )
